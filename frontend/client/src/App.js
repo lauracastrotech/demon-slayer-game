59d95f98758f28bbd6fd944c4664601f31a355ce
Laura Castro <lauracastrotech@gmail.com>
2024-09-27 12:34:16 -0400

    finished setting up frontend

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { fetchBackendData } from './api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [message, setMessage] = useState('');
+
+   useEffect(() => {
+      fetchBackendData()
+         .then(response => setMessage(response.data))
+         .catch(error => console.error('Error fetching data:', error));
+   }, []);
+
+   return (
+      <div>
+         <h1>{message}</h1>
+      </div>
+   );
 }
 
 export default App;
+
