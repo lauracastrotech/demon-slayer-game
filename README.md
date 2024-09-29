fdfa79b69bc1c2b97d8a263c6a22a9b9e8a654f5
Laura Castro <lauracastrotech@gmail.com>
2024-09-29 18:55:08 -0400

    created grid and basic styles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # demon-slayer-game
 This is a memory game to match Demon Slayer characters.
+
+Prep time => 90 min
+Development time => 4hrs 30min
+
---
+++ memory-game/client/src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  const [tiles, setTiles] = useState([1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]);
+
+  const [isMatched, setIsMatched] = useState(false);
+
+  const matches = 0;
+
+  useEffect(() => {
+     console.log('hello');
+    //  shuffle(tiles);
+  }, []);
+
+  // shuffle tiles
+  const shuffle = (tiles) => {
+     // copy tiles
+     let tilesCopy = [...tiles];
+
+     // reassign tile using random index
+     for(let tile = 15; tile >= 0; tile--){
+        tilesCopy[tile] = tilesCopy[Math.floor(Math.random() * 16)]
+     }
+     setTiles(tilesCopy);
+  }
+
+  // add event listener to flip tiles
+  const flipTile = (e) => {
+    e.preventDefault();
+  }
+
+  console.log("after useEffect load this is the state of tiles", tiles);
+  return (
+     <>
+        <div id="game">
+          <div id="matches">Matches: {matches}</div>
+           <div className = "grid-container" id="tile-board">
+            {tiles.map((tile, i) => {
+              return (
+                <div key = {i} className="grid-item" onClick={flipTile}>{tile}</div>
+              )
+            })}
+          </div>
+        </div>
+     </>
+  );
+}
+
+export default App;
