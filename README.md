c4c5179464f45a6a873f5078236dc5a4e6b49088
Laura Castro <lauracastrotech@gmail.com>
2024-09-29 20:54:02 -0400

    time ran out and wrote reflection

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ This is a memory game to match Demon Slayer characters.
 Prep time => 90 min
 Development time => 4hrs 30min
 
+Wanted to use Demon Slayer API, but it's missing a CORS header to allow cross-origin. I didn't know how to setup a local proxy to by-pass it.
+
+https://www.demonslayer-api.com/
+https://webtips.dev/memory-game-in-javascript 
+
+Reflection:
+Spent way too much time thiking about features
+Should have focused on the 3 must-have functionalities
+Improved understanding of CSS grid
+Understood Fisher-Yates shuffle algorithm, but wasn't successful in implementation
+Better understanding of axios and API requests
+Reinforced learning about useEffect and HOF
+Learned how to access the value of a div with data-value attribute
--- memory-game/client/src/App.jsx
+++ memory-game/client/src/App.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
-  const [tiles, setTiles] = useState([null,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]);
-
   const [isMatched, setIsMatched] = useState(false);
-
-  const matches = 0;
+  let tiles = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
+  let pair = [null, null];
+  let flippedTiles = 0;
+  let matches = 0;
 
   useEffect(() => {
-     console.log('hello');
+    console.log('hello');
+    //addCharacters();
     //  shuffle(tiles);
   }, []);
 
+  console.log("after useEffect load this is the state of tiles", tiles);
+
+  const addCharacters = async () => {
+    for(let i = 0; i < tiles.length; i+=2){
+      try {
+        if( i === 0) {
+          let tile1 = await axios.get(`https://www.demonslayer-api.com/api/v1/characters?/${i}`)
+          tiles[i] = response;
+          tiles[i+1] = response;
+        }
+      } catch(e) {
+        console.log(e.message);
+      }
+    }
+  }
   // shuffle tiles
   const shuffle = (tiles) => {
      // copy tiles
@@ -24,15 +41,48 @@ function App() {
      for(let tile = 15; tile >= 0; tile--){
         tilesCopy[tile] = tilesCopy[Math.floor(Math.random() * 16)]
      }
-     setTiles(tilesCopy);
+     tiles = tilesCopy;
+  }
+
+  // check if the two tiles match
+  const checkTiles = (pair) => {
+    // do the two tiles match
+    if(pair[0] === pair[1]){
+      setIsMatched(true);
+    } else {
+      flipDown();
+    }
+    console.log("The value of pair in checkTiles func", pair);
   }
 
   // add event listener to flip tiles
-  const flipTile = (e) => {
+  const flipUp = (e) => {
     e.preventDefault();
+    // get the value of the tile
+    let tile = e.target.getAttribute('data-value');
+  
+    // add the value of flipped tile to pair array
+    pair[flippedTiles] = tile;
+
+    // counter for flipped tiles
+    flippedTiles++;
+    
+    // check if they match when two tiles have been flipped
+    if(flippedTiles === 2){
+      checkTiles(pair);
+    }
   }
 
-  console.log("after useEffect load this is the state of tiles", tiles);
+  // if the tiles don't match, flip both down
+  const flipDown = () => {
+
+  }
+
+  // reset game
+  const resetGame = () => {
+    shuffle(tiles);
+  }
+
   return (
      <>
         <div id="game">
@@ -40,11 +90,11 @@ function App() {
            <div className = "grid-container" id="tile-board">
             {tiles.map((tile, i) => {
               return (
-                <div key = {i} className="grid-item" onClick={flipTile}>{tile}</div>
+                <div key = {i} data-value={tile} className="grid-item" onClick={flipUp}>{tile}</div>
               )
             })}
           </div>
-          <button>Reset</button>
+          <button onClick={resetGame}>Reset</button>
         </div>
      </>
   );
